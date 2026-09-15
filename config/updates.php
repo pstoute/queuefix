d93e4732b48eab2d20628ee53e7c82879f040a22
Paul Stoute <paul@stoute.co>
2026-09-15 10:23:53 -0700

    Prepare v1.1.2 release

--- config/updates.php
+++ config/updates.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'version' => env('QUEUEFIX_VERSION', 'v1.1.1'),
+    'version' => 'v1.1.2',
     'repository' => env('QUEUEFIX_UPDATE_REPOSITORY', 'pstoute/queuefix'),
     'cache_hours' => (int) env('QUEUEFIX_UPDATE_CACHE_HOURS', 12),
     'timeout_seconds' => (int) env('QUEUEFIX_UPDATE_TIMEOUT_SECONDS', 3),
